cd087b382e499e0aafd20a9ccd3769ecae3ff2e6
creativekamrul <kamrulalif2004@gmail.com>
2021-12-20 14:38:05 +0600

    All Fixed

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -5,7 +5,7 @@ const SingleColor = ({colorData, index}) => {
     const newRgb = colorData.rgb.join(",")
     const background = colorData.hex
     return (
-        <div className={index > 10 ? `single_color text_light`: `single_color`} style={{backgroundColo: `rgb(${newRgb})`}}>
+        <div className={index > 10 ? `single_color text_light`: `single_color`} style={{backgroundColor: `rgb(${newRgb})`}}>
             <h3># {background}</h3>
             <h3>{colorData.weight}%</h3>
         </div>
